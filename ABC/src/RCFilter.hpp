42ec6eff545ff99814a797fdc4c4759243946b0c
LOGUNIVPM <l.gabrielli@staff.univpm.it>
2020-02-11 23:16:15 +0100

    added authorship to all source files

--- ABC/src/RCFilter.hpp
+++ ABC/src/RCFilter.hpp
@@ -1,3 +1,15 @@
+/*--------------------------- ABC ---------------------------------*
+ *
+ * Author: Leonardo Gabrielli <l.gabrielli@univpm.it>
+ * License: GPLv3
+ *
+ * For a detailed guide of the code and functions see the book:
+ * "Developing Virtual Synthesizers with VCV Rack" by L.Gabrielli
+ *
+ * Copyright 2020, Leonardo Gabrielli
+ *
+ *-----------------------------------------------------------------*/
+
 #include "rack.hpp"
 
 template <typename T>
